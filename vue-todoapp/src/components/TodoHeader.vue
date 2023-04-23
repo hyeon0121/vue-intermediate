53cbf45fea65fde15db4a9c98ee8d7981f706ea1
hyeon0121 <dpwl1172@naver.com>
2023-04-23 14:21:40 +0900

    Add todoapp project

---
+++ vue-todoapp/src/components/TodoHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1>
+      TODO It!
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+h1 {
+  color : #2f3852;
+  font-weight : 900;
+  margin : 2.5rem 0 1.5rem;
+}
+</style>
---
+++ vue-todoapp/src/components/TodoInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" @keyup.enter="addTodo">
+    <span class="addContainer" @click="addTodo">
+      <i class="fas fa-plus addBtn "></i>
+    </span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: function() {
+    return {
+      newTodoItem : ""
+    }
+  },
+  methods: {
+    addTodo() {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput() {
+      this.newTodoItem = "";
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
